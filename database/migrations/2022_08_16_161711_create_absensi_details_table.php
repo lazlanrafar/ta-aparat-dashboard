797ed02f8895a58aff03f4cc1ea07d0dee677667
lazlanrafar <lazlanrafar@gmail.com>
2022-08-16 23:18:55 +0700

    migrate and model absen detail

---
+++ database/migrations/2022_08_16_161711_create_absensi_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('absensi_details', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_absensi');
+            $table->integer('nip');
+            $table->string('nama');
+            $table->string('jabatan');
+            $table->string('ttd');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('absensi_details');
+    }
+};
