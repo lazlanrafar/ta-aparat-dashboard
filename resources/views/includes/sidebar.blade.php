104a66b90cff8ad801c312d1f192c6761afc9f77
lazlanrafar <lazlanrafar@gmail.com>
2022-08-22 15:17:33 +0700

    handle level

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -17,42 +17,52 @@
                         <p>Dashboard</p>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a href="/pegawai" class="nav-link {{ Request::is('pegawai') ? 'active' : '' }}">
-                        <i class="nav-icon fas fa-users"></i>
-                        <p>Pegawai</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/ruang-rapat" class="nav-link {{ Request::is('ruang-rapat') ? 'active' : '' }}">
-                        <i class="nav-icon fas fa-table"></i>
-                        <p>Ruang Rapat</p>
-                    </a>
-                </li>
+                @if (Auth::user()->level == 'Administrasi Umum')
+                    <li class="nav-item">
+                        <a href="/pegawai" class="nav-link {{ Request::is('pegawai') ? 'active' : '' }}">
+                            <i class="nav-icon fas fa-users"></i>
+                            <p>Pegawai</p>
+                        </a>
+                    </li>
+                @endif
+                @if (Auth::user()->level == 'Administrasi Umum')
+                    <li class="nav-item">
+                        <a href="/ruang-rapat" class="nav-link {{ Request::is('ruang-rapat') ? 'active' : '' }}">
+                            <i class="nav-icon fas fa-table"></i>
+                            <p>Ruang Rapat</p>
+                        </a>
+                    </li>
+                @endif
                 <li class="nav-item">
                     <a href="/peminjaman" class="nav-link {{ Request::is('peminjaman') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-users"></i>
                         <p>Peminjaman</p>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a href="/notulen" class="nav-link {{ Request::is('notulen') ? 'active' : '' }}">
-                        <i class="nav-icon fas fa-edit"></i>
-                        <p>Notulen</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/absensi" class="nav-link {{ Request::is('absensi') ? 'active' : '' }}">
-                        <i class="nav-icon fas fa-book"></i>
-                        <p>Absensi</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="/laporan" class="nav-link {{ Request::is('laporan') ? 'active' : '' }}">
-                        <i class="nav-icon fas fa-book"></i>
-                        <p>Laporan</p>
-                    </a>
-                </li>
+                @if (Auth::user()->level == 'Pegawai')
+                    <li class="nav-item">
+                        <a href="/notulen" class="nav-link {{ Request::is('notulen') ? 'active' : '' }}">
+                            <i class="nav-icon fas fa-edit"></i>
+                            <p>Notulen</p>
+                        </a>
+                    </li>
+                @endif
+                @if (Auth::user()->level == 'Pegawai')
+                    <li class="nav-item">
+                        <a href="/absensi" class="nav-link {{ Request::is('absensi') ? 'active' : '' }}">
+                            <i class="nav-icon fas fa-book"></i>
+                            <p>Absensi</p>
+                        </a>
+                    </li>
+                @endif
+                @if (Auth::user()->level != 'Pegawai')
+                    <li class="nav-item">
+                        <a href="/laporan" class="nav-link {{ Request::is('laporan') ? 'active' : '' }}">
+                            <i class="nav-icon fas fa-book"></i>
+                            <p>Laporan</p>
+                        </a>
+                    </li>
+                @endif
             </ul>
         </nav>
     </div>
