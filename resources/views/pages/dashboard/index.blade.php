ebf057eadbb86efd796859379ee6b71eb4f31e47
lazlanrafar <lazlanrafar@gmail.com>
2022-08-22 14:37:38 +0700

    dashboard row 1

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -14,6 +14,47 @@
             <div class="row">
                 @include('includes.error-card')
             </div>
+            <div class="row ">
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-info">
+                        <div class="inner">
+                            <h3>{{ $total_pegawai }}</h3>
+
+                            <p>Pegawai</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-android-people"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                        <div class="inner">
+                            <h3>{{ $total_ruangan }}</h3>
+
+                            <p>Ruang Rapat</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-grid"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3>{{ $total_peminjaman }}</h3>
+
+                            <p>Peminjaman</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-calendar"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
